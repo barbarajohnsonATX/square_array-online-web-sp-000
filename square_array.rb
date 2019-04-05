0bc4b28c70d292aa35ac935831ce3981c4227206
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-05 00:00:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_array = Array.new
   array.each do |element|
-    new_array.push(element*element)
+    new_array.push(element**2)
     binding.pry
   end 
 end
